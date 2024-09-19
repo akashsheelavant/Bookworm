8997e855d3cc9c18fba6b3ee4a30cb5c297a5b79
Akash Sheelavant <asheelavant@walmart.com>
2024-09-19 13:05:58 +0530

    Initial project.

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Akash Sheelavant on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
